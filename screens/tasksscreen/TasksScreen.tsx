b1bfa27fe8660dd29a024d80dac1a984dbf360a7
AngelinaOberkofler <obeanc20@htl-kaindorf.at>
2024-02-01 14:48:52 +0100

    change again

--- screens/tasksscreen/TasksScreen.tsx
+++ screens/tasksscreen/TasksScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Platform, Modal } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Platform, Modal, ScrollView } from 'react-native';
 import styles from './TasksScreenStyles';
 
 const TasksScreen: React.FC = ({ navigation }: any) => {
@@ -29,75 +29,11 @@ const TasksScreen: React.FC = ({ navigation }: any) => {
     <View style={styles.container}>
       <Text style={styles.title}>Tasks Screen</Text>
       
-      <Text>
-
-        
-      </Text>
       <TouchableOpacity style={styles.taskButton} onPress={() => handleTaskPress('Task1')}>
         <Text style={styles.taskButtonText}>Task 1</Text>
       </TouchableOpacity>
       
       <TouchableOpacity style={styles.taskButton} onPress={() => handleTaskPress('Task2')}>
-        <Text style={styles.taskButtonText}>Task 2</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.taskButton} onPress={() => handleTaskPress('Task3')}>
-        <Text style={styles.taskButtonText}>Task 3</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.taskButton} onPress={() => handleTaskPress('Task4')}>
-        <Text style={styles.taskButtonText}>Task 4</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.taskButton} onPress={() => handleTaskPress('Task5')}>
-        <Text style={styles.taskButtonText}>Task 5</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.taskButton} onPress={() => handleTaskPress('Task6')}>
-        <Text style={styles.taskButtonText}>Task </Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.taskButton} onPress={() => handleTaskPress('Task7')}>
-        <Text style={styles.taskButtonText}>Task 2</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.taskButton} onPress={() => handleTaskPress('Task8')}>
-        <Text style={styles.taskButtonText}>Task 2</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.taskButton} onPress={() => handleTaskPress('Task9')}>
-        <Text style={styles.taskButtonText}>Task 2</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.taskButton} onPress={() => handleTaskPress('Task10')}>
-        <Text style={styles.taskButtonText}>Task 2</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.taskButton} onPress={() => handleTaskPress('Task11')}>
-        <Text style={styles.taskButtonText}>Task 2</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.taskButton} onPress={() => handleTaskPress('Task12')}>
-        <Text style={styles.taskButtonText}>Task 2</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.taskButton} onPress={() => handleTaskPress('Task13')}>
-        <Text style={styles.taskButtonText}>Task 2</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.taskButton} onPress={() => handleTaskPress('Task14')}>
-        <Text style={styles.taskButtonText}>Task 2</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.taskButton} onPress={() => handleTaskPress('Task15')}>
-        <Text style={styles.taskButtonText}>Task 2</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.taskButton} onPress={() => handleTaskPress('Task16')}>
-        <Text style={styles.taskButtonText}>Task 2</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.taskButton} onPress={() => handleTaskPress('Task17')}>
         <Text style={styles.taskButtonText}>Task 2</Text>
       </TouchableOpacity>
 
