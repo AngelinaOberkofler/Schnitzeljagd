37f17e076053a4f54d1ccbb5d692b773431c974f
zhuvec20 <zhuvec20@htl-kaindorf.at>
2024-03-07 13:57:40 +0100

    tasks beginning

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/homescreen/HomeScreen';
 import TasksScreen from './screens/tasksscreen/TasksScreen';
+import TaskDetails from './screens/tasksscreen/TaskDetails';
 import ResultScreen from './screens/resultscreen/ResultScreen';
 
 
@@ -14,6 +15,7 @@ const App: React.FC = () => {
             <Stack.Navigator>
                 <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
                 <Stack.Screen name="Tasks" component={TasksScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="TaskDetails" component={TaskDetails} options={{ headerShown: false }}/>
                 <Stack.Screen name="ResultScreen" component={ResultScreen} options={{ headerShown: false }}/>
             </Stack.Navigator>
         </NavigationContainer>
---
+++ screens/tasksscreen/tasks/Verheiratet.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Platform,
+  Modal,
+  ScrollView,
+  TextInput,
+} from "react-native";
+import { RadioButton } from "react-native-paper";
+import styles from "../TasksScreenStyles";
+
+const Verheiratet: React.FC = () => {
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [checked, setChecked] = useState<string>();
+
+  const checkAnswer = () => {
+    const correctAnswer = true;
+    setIsCorrect(checked === correctAnswer.toString());
+  };
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.questionText}>
+        Finde heraus, ob der Leiter der Informatikabteilung verheiratet ist.
+      </Text>
+      <View style={styles.radioGroup}>
+        <View style={styles.radioButton}>
+          <RadioButton
+            value="true"
+            status={checked === "true" ? "checked" : "unchecked"}
+            onPress={() => setChecked("true")}
+          />
+          <Text style={styles.radioLabel} onPress={() => setChecked("true")}>
+            Wahr
+          </Text>
+        </View>
+
+        <View style={styles.radioButton}>
+          <RadioButton
+            value="false"
+            status={checked === "false" ? "checked" : "unchecked"}
+            onPress={() => setChecked("false")}
+          />
+          <Text style={styles.radioLabel} onPress={() => setChecked("false")}>
+            Falsch
+          </Text>
+        </View>
+      </View>
+      <TouchableOpacity style={styles.modalButton} onPress={checkAnswer}>
+        <Text style={styles.modalButtonText}>Antwort überprüfen</Text>
+      </TouchableOpacity>
+      {isCorrect && <Text style={styles.modalText}>Richtig!</Text>}
+    </ScrollView>
+  );
+};
+
+export default Verheiratet;
